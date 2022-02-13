c36bf831e56701259247d4c19dd49138f1a80667
DoguD <dogudeniz.ugur@gmail.com>
2022-02-13 14:27:23 +0100

    3rd reward dist done

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -184,7 +184,7 @@ export default function Dashboard() {
     async function getShareHolderCount() {
         let count = 0;
         let checker = 0;
-        for (let i = 1270; i < 2000; i++) {
+        for (let i = 1550; i < 2500; i++) {
             try {
                 await easyBlockContract.holders(i);
                 checker = 0;
@@ -659,7 +659,7 @@ export default function Dashboard() {
                                         {generalDataLoading ?
                                             <Spinner/> :
                                             <StatNumber fontSize="md" color={textColor}>
-                                                {dollarUSLocale.format((2266.7521 + 13238).toFixed(2))} $
+                                                {dollarUSLocale.format((2266.7521 + 13238 + 21102).toFixed(2))} $
                                             </StatNumber>}
                                     </Flex>
                                 </Stat>
@@ -973,7 +973,7 @@ export default function Dashboard() {
                                         color: "#3e68a4",
                                         marginTop: 8,
                                         textAlign: 'center',
-                                    }}>Next Reward Distribution:<br/>February 13, 2022<br/>
+                                    }}>Next Reward Distribution:<br/>February 20, 2022<br/>
                                         {userDataLoading ? <Spinner/> : <span
                                             style={{fontWeight: 'normal', fontSize: 14}}>Your share from the generated revenue will be directly deposited into your wallet every 7 days.</span>}
                                     </Text>
