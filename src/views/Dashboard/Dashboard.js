3708e00aa76d75a97a09230c2474852e4d29d960
DoguD <dogudeniz.ugur@gmail.com>
2022-03-11 20:37:43 +0100

    Buy shares box UI fix

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1124,7 +1124,7 @@ export default function Dashboard() {
                             bgPosition="center"
                             bgRepeat="no-repeat"
                             w="100%"
-                            h={{sm: "500px", lg: "400px"}}
+                            h={{sm: "340px", lg: "400px"}}
                             bgSize="cover"
                             position="relative"
                             borderRadius="15px"
