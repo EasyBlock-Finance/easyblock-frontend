adecdce89223af30bbcd8d958811f5ff4a23b187
DoguD <dogudeniz.ugur@gmail.com>
2022-05-10 21:23:11 +0200

    per share decimals fix

--- src/components/Dashboard/ExplanationBox.js
+++ src/components/Dashboard/ExplanationBox.js
@@ -40,7 +40,7 @@ export default function ExplanationBox(props) {
                                 style={{fontWeight: 'normal', color: "#000000", fontSize: 16}}>
                             EasyBlock is a protocol running on Fantom Network which enables investors of all sizes to invest in StrongBlock Nodes. StrongBlock requires a minimum investment
                             amount of <b>${(props.strongPrice * 10).toFixed(0)}</b> and runs on Ethereum Mainnet which has really high gas-fees. These conditions make StrongBlock inaccesible for most of investors.
-                            On contrary, <b>EasyBlock</b> has a minimum investment amount of <b>${props.sharePrice * 100}</b> per share and minimal gas fees. <br/>
+                            On contrary, <b>EasyBlock</b> has a minimum investment amount of <b>${(props.sharePrice * 100).toFixed(2)}</b> per share and minimal gas fees. <br/>
                             The amount generated from share sales are bridged to Ethereum Mainnet and used to purchase StrongBlock Nodes. Revenue generated from those nodes is bridged back to Fantom on optimal intervals and distributed to shareholders.
                         </span>
                 <Button
